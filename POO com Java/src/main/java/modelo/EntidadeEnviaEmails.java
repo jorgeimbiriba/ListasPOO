deaa3b9fad81197f583c4108d508f8e0bb4717a5
Jorge Henrique <jorge.henrique7171@gmail.com>
2024-11-16 09:29:24 -0400

    Organização de Pastas

--- POO com Java/src/main/java/modelo/EntidadeEnviaEmails.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public interface EntidadeEnviaEmails {
-
-  public abstract String prepararConteudoEmail();
-   //Classe abstratta com métodos abstratos pode ser substituída pelo métod interface
-}
